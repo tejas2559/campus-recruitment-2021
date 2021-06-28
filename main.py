b3eb8992559e401b6c0977497e3b47518a137f18
tejas2559 <73305005+tejas2559@users.noreply.github.com>
2021-06-28 13:30:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-a1=[1, 5, 8, 9, 10];
-a2=[5, 8, 9, 10, 12, 20, 40, 60, 70];
-num=int(input("enter the number to check:")); #input the number to check
-a3=0
-a4=0
-if num in a1: #check if num in a1
-    a3=1
-if num in a2: #check if num in a2
-        a4=1
-        if a3==a4:  # num in a1 and a2 print if no then skip
-            print("number", str(num) + "found in both array")
-elif num in a1: # ii num in a1 print
-    print("number",str(num)+"found in array_one")
-elif num in a2: #if number in a2 print
-    print("number",str(num)+ "found in array_two",)
+s = input("Enter value: ")
 
-
-
-
-
-
-
-
+l1=[c for c in s]   # in Python, a string is just a sequence, so we can iterate over it!
+l2=[ord(c) for c in s]
+print(l1)
+print(l2)
+#
