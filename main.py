26641981bffe36c606f82de05b10b10be933be81
tejas2559 <73305005+tejas2559@users.noreply.github.com>
2021-06-28 12:00:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+a1=[1, 5, 8, 9, 10];
+a2=[5, 8, 9, 10, 12, 20, 40, 60, 70];
+num=int(input("enter the number to check:"));
+if num in a1:
+    print("number",str(num)+"found in array_one")
+if num in a2:
+    print("number",str(num)+ "found in array_two",)
+
+
+
+
+
+
