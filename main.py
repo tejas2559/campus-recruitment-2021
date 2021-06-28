37de64558c8129c00d8ef34ea00b1983f94c0756
tejas2559 <73305005+tejas2559@users.noreply.github.com>
2021-06-28 12:32:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 a1=[1, 5, 8, 9, 10];
 a2=[5, 8, 9, 10, 12, 20, 40, 60, 70];
-num=int(input("enter the number to check:"));
+num=int(input("enter the number to check:"));#input the number to check
 a3=0
 a4=0
-if num in a1:
+if num in a1:#check if num in a1
     a3=1
-if num in a2:
+if num in a2:#check if num in a2
         a4=1
-        if a3==a4:
+        if a3==a4:# num in a1 and a2 print if no then skip
             print("number", str(num) + "found in both array")
-elif num in a1:
+elif num in a1:# ii num in a1 print
     print("number",str(num)+"found in array_one")
-elif num in a2:
+elif num in a2:#if number in a2 print
     print("number",str(num)+ "found in array_two",)
 
 
