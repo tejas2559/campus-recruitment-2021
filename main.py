955c746ba04acc0d55904632e5ae2f3d8322606c
tejas2559 <73305005+tejas2559@users.noreply.github.com>
2021-06-28 13:37:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-s = input("Enter value: ")
+a1=[1, 5, 8, 9, 10];
+a2=[5, 8, 9, 10, 12, 20, 40, 60, 70];
+num=int(input("enter the number to check:"));
+a3=0
+a4=4
+if num in a1:
+    a3=1
+     if num in a2:
+        a4=1
+    if a3==a4:
+        print("number",str(num)+"found in array_one")
+elif num in a2:
+    print("number",str(num)+ "found in array_two",)
 
-l1=[c for c in s]   # in Python, a string is just a sequence, so we can iterate over it!
-l2=[ord(c) for c in s]
-print(l1)
-print(l2)
-#
+elif num in a1:
+    print("number",str(num)+"found in array_one")
+elif num in a2:
+    print("number",str(num)+ "found in array_two",)
